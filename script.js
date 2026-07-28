d2fff203905b93412aaeec016d77ae883cb823ed
Beni <vescienblake@gmail.com>
2026-07-28 10:14:28 -0300

    Add randomColor() function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+
 let containerSize = 600;
 container.style.width = `${containerSize}px`;
 container.style.height = `${containerSize}px`;
@@ -14,8 +15,18 @@ function createGrid(gridSize, boxSize) {
             box.style.height = `${boxSize}px`;
             box.style.width = `${boxSize}px`;
             container.appendChild(box); 
+            box.addEventListener("mouseover", () => box.style.backgroundColor = randomColor());
         }
     }
+}
+
+function randomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 createGrid(gridSize, boxSize);
